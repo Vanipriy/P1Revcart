ec886b03b9425376b947023e51a9d00d66c2ef5c
Vanipriy <vani05102002@gmail.com>
2025-12-07 16:12:56 +0530

    Initial push - Full Revcart Project

---
+++ backend/src/main/java/com/revcart/security/OAuth2SuccessHandler.java
@@ -0,0 +1,44 @@
+package com.revcart.security;
+
+import com.revcart.entity.User;
+import com.revcart.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        User user = userRepository.findByEmail(email).orElseGet(() -> {
+            User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setName(name);
+            newUser.setRole(User.Role.CUSTOMER);
+            newUser.setEmailVerified(true);
+            newUser.setPassword("");
+            return userRepository.save(newUser);
+        });
+
+        String token = jwtUtils.generateTokenFromEmail(email);
+        response.sendRedirect("http://localhost:4200/oauth2/redirect?token=" + token);
+    }
+}
